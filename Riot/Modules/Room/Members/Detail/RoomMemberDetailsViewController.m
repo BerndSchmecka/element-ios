1edb495dc3a530870e3d81fc1696cdc5bc1912a6
Doug <douglase@element.io>
2021-12-02 14:25:45 +0000

    Add AnalyticsScreenTimer and track more screens.
    Update Analytics with new methods in MXAnalyticsDelegate.

--- Riot/Modules/Room/Members/Detail/RoomMemberDetailsViewController.m
+++ Riot/Modules/Room/Members/Detail/RoomMemberDetailsViewController.m
@@ -102,6 +102,8 @@
 
 @property(nonatomic, strong) UserVerificationCoordinatorBridgePresenter *userVerificationCoordinatorBridgePresenter;
 
+@property(nonatomic) AnalyticsScreenTimer *screenTimer;
+
 @end
 
 @implementation RoomMemberDetailsViewController
@@ -137,6 +139,8 @@
     
     // Keep visible the status bar by default.
     isStatusBarHidden = NO;
+    
+    self.screenTimer = [[AnalyticsScreenTimer alloc] initWithScreen:AnalyticsScreenUser];
 }
 
 - (void)viewDidLoad
@@ -243,9 +247,6 @@
 {
     [super viewWillAppear:animated];
 
-    // Screen tracking
-    [Analytics.shared trackScreen:AnalyticsScreenUser];
-
     [self userInterfaceThemeDidChange];
 
     // Hide the bottom border of the navigation bar to display the expander header
@@ -266,6 +267,18 @@
     [self hideNavigationBarBorder:NO];
     
     self.bottomImageView.hidden = YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.screenTimer start];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    [self.screenTimer stop];
 }
 
 - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator
